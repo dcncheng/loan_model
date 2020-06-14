65a9ed12029132edf53cbee7b29792911f969161
dcncheng <dennis.cheng@centurylink.com>
2020-06-14 16:50:26 -0500

    debug running MLProject from GitHub

--- train_model.py
+++ train_model.py
@@ -36,28 +36,30 @@ def run_training() -> None:
     # Set MLFlow experiment
     mlflow.set_experiment(config.EXPERIMENT_NAME)
 
-    loan_pipe.fit(X_train[config.FEATURES], y_train)
+    with mlflow.start_run():
+        loan_pipe.fit(X_train[config.FEATURES], y_train)
 
-    # Log dataset and parameters to mlflow
-    dataset_full_path_name = dataset_location()
-    mlflow.set_tag("dataset", dataset_full_path_name)
+        # Log dataset
+        dataset_full_path_name = dataset_location()
+        mlflow.set_tag("dataset", dataset_full_path_name)
 
-    mlflow.log_param("penalty", model_config.PENALTY)
-    mlflow.log_param("dual", model_config.DUAL)
-    mlflow.log_param("C", model_config.C)
-    mlflow.log_param("fit_intercept", model_config.FIT_INTERCEPT)
-    mlflow.log_param("random_state", model_config.RANDOM_STATE)
-    mlflow.log_param("class_weight", model_config.CLASS_WEIGHT)
-    mlflow.log_param("max_iter", model_config.MAX_ITER)
-    mlflow.log_param("multi_class", model_config.MULTI_CLASS)
+        # Log parameters
+        mlflow.log_param("penalty", model_config.PENALTY)
+        mlflow.log_param("dual", model_config.DUAL)
+        mlflow.log_param("C", model_config.C)
+        mlflow.log_param("fit_intercept", model_config.FIT_INTERCEPT)
+        mlflow.log_param("random_state", model_config.RANDOM_STATE)
+        mlflow.log_param("class_weight", model_config.CLASS_WEIGHT)
+        mlflow.log_param("max_iter", model_config.MAX_ITER)
+        mlflow.log_param("multi_class", model_config.MULTI_CLASS)
 
-    # Log the metrics
-    score = loan_pipe.score(X_train, y_train)
-    mlflow.log_metric("score", score)
+        # Log the metrics
+        score = loan_pipe.score(X_train, y_train)
+        mlflow.log_metric("score", score)
 
-    # Save the sklearn pipeline as mlflow model
-    conda_env = mlflow.sklearn.get_default_conda_env()
-    mlflow.sklearn.log_model(loan_pipe, "sklearn_pipeline", conda_env=conda_env)
+        # Save the sklearn pipeline as mlflow model
+        conda_env = mlflow.sklearn.get_default_conda_env()
+        mlflow.sklearn.log_model(loan_pipe, "sklearn_pipeline", conda_env=conda_env)
 
 if __name__ == "__main__":
     run_training()
